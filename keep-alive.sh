8c99deecb396af21ff7b7b30dbbadfae695fe38c
Z User <z@container>
2026-05-06 12:25:13 +0000

    e92858cd-fc05-4996-abbd-f8b3bfb887b8

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd /home/z/my-project
-LOG=dev.log
 while true; do
-    node .next/standalone/server.js -p 3000 >> $LOG 2>&1
-    echo "[$(date)] Restarting server..." >> $LOG
-    sleep 2
+  if ! curl -s -o /dev/null -w "%{http_code}" --max-time 3 http://localhost:3000/ 2>/dev/null | grep -q "200"; then
+    fuser -k 3000/tcp 2>/dev/null
+    sleep 1
+    node .next/standalone/server.js &>/dev/null &
+  fi
+  sleep 5
 done
